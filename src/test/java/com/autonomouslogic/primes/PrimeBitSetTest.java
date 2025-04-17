70fcea0d0b28dce116bf0cb70ee99c81c3ca5af2
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-04-17 14:58:19 +0900

    Sieves with initial prime list (#7)

--- src/test/java/com/autonomouslogic/primes/PrimeBitSetTest.java
+++ src/test/java/com/autonomouslogic/primes/PrimeBitSetTest.java
@@ -17,47 +17,67 @@ public class PrimeBitSetTest {
 	@ParameterizedTest
 	@MethodSource("addressingTests")
 	void shouldConvertNumbersToAddresses(long number, int address) {
-		assertEquals(address, PrimeBitSet.numberToAddress(number));
+		assertEquals(address, PrimeBitSet.numberToAddress(30, number + 30));
 	}
 
 	@ParameterizedTest
 	@MethodSource("addressingTests")
 	void shouldConvertAddressesToNumbers(long number, int address) {
-		assertEquals(number, PrimeBitSet.addressToNumber(address));
+		assertEquals(number + 30, PrimeBitSet.addressToNumber(30, address));
+	}
+
+	@ParameterizedTest
+	@MethodSource("addressingTests")
+	void shouldConvertNumbersToAddressesWithOffsets(long number, int address) {
+		assertEquals(address, PrimeBitSet.numberToAddress(300, number + 300));
+	}
+
+	@ParameterizedTest
+	@MethodSource("addressingTests")
+	void shouldConvertAddressesToNumbersWithOffsets(long number, int address) {
+		assertEquals(number + 300, PrimeBitSet.addressToNumber(300, address));
 	}
 
 	static Stream<Arguments> addressingTests() {
 		return Stream.of(
-				Arguments.of(31, 0),
-				Arguments.of(37, 1),
-				Arguments.of(41, 2),
-				Arguments.of(43, 3),
-				Arguments.of(47, 4),
-				Arguments.of(49, 5),
-				Arguments.of(53, 6),
-				Arguments.of(59, 7),
-				Arguments.of(61, 8),
-				Arguments.of(67, 9),
-				Arguments.of(71, 10),
-				Arguments.of(73, 11),
-				Arguments.of(77, 12),
-				Arguments.of(79, 13),
-				Arguments.of(83, 14),
-				Arguments.of(89, 15),
-				Arguments.of(91, 16),
-				Arguments.of(97, 17),
-				Arguments.of(101, 18),
-				Arguments.of(103, 19),
-				Arguments.of(107, 20),
-				Arguments.of(109, 21),
-				Arguments.of(113, 22),
-				Arguments.of(119, 23));
+				Arguments.of(1, 0),
+				Arguments.of(7, 1),
+				Arguments.of(11, 2),
+				Arguments.of(13, 3),
+				Arguments.of(17, 4),
+				Arguments.of(19, 5),
+				Arguments.of(23, 6),
+				Arguments.of(29, 7),
+				Arguments.of(31, 8),
+				Arguments.of(37, 9),
+				Arguments.of(41, 10),
+				Arguments.of(43, 11),
+				Arguments.of(47, 12),
+				Arguments.of(49, 13),
+				Arguments.of(53, 14),
+				Arguments.of(59, 15),
+				Arguments.of(61, 16),
+				Arguments.of(67, 17),
+				Arguments.of(71, 18),
+				Arguments.of(73, 19),
+				Arguments.of(77, 20),
+				Arguments.of(79, 21),
+				Arguments.of(83, 22),
+				Arguments.of(89, 23));
 	}
 
 	@Test
-	void shouldReturnMaxNumber() {
-		assertEquals(59L, new PrimeBitSet(1).getLastNumber());
-		assertEquals(89L, new PrimeBitSet(2).getLastNumber());
+	void shouldReturnFirstNumber() {
+		assertEquals(2L, new PrimeBitSet(1).getFirstNumber());
+		assertEquals(2L, new PrimeBitSet(100).getFirstNumber());
+		assertEquals(61L, new PrimeBitSet(60, 1).getFirstNumber());
+	}
+
+	@Test
+	void shouldReturnLastNumber() {
+		assertEquals(30 + 29, new PrimeBitSet(1).getLastNumber());
+		assertEquals(2 * 30 + 29, new PrimeBitSet(2).getLastNumber());
+		assertEquals(3 * 30 + 29, new PrimeBitSet(60, 2).getLastNumber());
 	}
 
 	@ParameterizedTest
@@ -89,14 +109,38 @@ public class PrimeBitSetTest {
 	@Test
 	void shouldOutputPrimes() {
 		var bits = new PrimeBitSet(1 << 20);
+		assertEquals(2, bits.getFirstNumber());
+		assertEquals((1 << 20) * 30 + 29, bits.getLastNumber());
 		var last = PRIMES[PRIMES.length - 1];
-		for (int i = 0; i <= last; i++) {
+		for (int i = 2; i <= last; i++) {
 			if (Arrays.binarySearch(PRIMES, i) < 0) {
 				bits.setIsNotPrime(i);
 			}
 		}
 		assertEquals(
 				Arrays.stream(PRIMES).mapToObj(String::valueOf).collect(Collectors.joining("\n")),
+				bits.primeStream()
+						.takeWhile(n -> n <= last)
+						.mapToObj(String::valueOf)
+						.collect(Collectors.joining("\n")));
+	}
+
+	@Test
+	void shouldOutputPrimesWithOffset() {
+		var bits = new PrimeBitSet(3000, 1 << 20);
+		assertEquals(3000 + 1, bits.getFirstNumber());
+		assertEquals(((1 << 20) - 1) * 30 + 29 + 3000, bits.getLastNumber());
+		var last = PRIMES[PRIMES.length - 1];
+		for (int i = 3001; i <= last; i++) {
+			if (Arrays.binarySearch(PRIMES, i) < 0) {
+				bits.setIsNotPrime(i);
+			}
+		}
+		assertEquals(
+				Arrays.stream(PRIMES)
+						.filter(p -> p > 3000)
+						.mapToObj(String::valueOf)
+						.collect(Collectors.joining("\n")),
 				bits.primeStream()
 						.takeWhile(n -> n <= last)
 						.mapToObj(String::valueOf)
--- src/test/java/com/autonomouslogic/primes/SieveOfEratosthenesTest.java
+++ src/test/java/com/autonomouslogic/primes/SieveOfEratosthenesTest.java
@@ -9,11 +9,20 @@ import org.junit.jupiter.api.Test;
 
 public class SieveOfEratosthenesTest {
 	@Test
+	void shouldSieve() {
+		var primes = new SieveOfEratosthenes(16 << 10).run();
+		var test = new TrialDivision();
+		primes.forEach(prime -> {
+			assertTrue(test.isPrime(prime), String.valueOf(prime));
+		});
+	}
+
+	@Test
 	void shouldSieveKnownPrimes() {
 		var expectedPrimes = Arrays.stream(PrimeList.PRIMES).boxed().toList();
 		var lastExpectedPrime = expectedPrimes.getLast();
 		var sieve = new SieveOfEratosthenes(1 << 20);
-		assertTrue(sieve.lastNumber() >= lastExpectedPrime);
+		assertTrue(sieve.getLastNumber() >= lastExpectedPrime);
 		var primes = sieve.run().takeWhile(p -> p <= lastExpectedPrime).boxed().toList();
 		assertEquals(
 				expectedPrimes.stream().map(String::valueOf).collect(Collectors.joining("\n")),
@@ -21,11 +30,23 @@ public class SieveOfEratosthenesTest {
 	}
 
 	@Test
-	void shouldSieve() {
-		var primes = new SieveOfEratosthenes(16 << 10).run();
-		var test = new TrialDivision();
-		primes.forEach(prime -> {
-			assertTrue(test.isPrime(prime), String.valueOf(prime));
-		});
+	void shouldSieveKnownPrimesWithInitialList() {
+		var offset = 900;
+		var expectedPrimes = Arrays.stream(PrimeList.PRIMES).boxed().toList();
+		var lastExpectedPrime = expectedPrimes.getLast();
+
+		var initPrimes =
+				Arrays.stream(PrimeList.PRIMES).takeWhile(n -> n < offset).asLongStream();
+		var sieve = new SieveOfEratosthenes(offset, 1 << 20);
+		assertTrue(sieve.getLastNumber() >= lastExpectedPrime);
+		sieve.init(initPrimes);
+
+		var primes = sieve.run().takeWhile(p -> p <= lastExpectedPrime).boxed().toList();
+		assertEquals(
+				expectedPrimes.stream()
+						.filter(n -> n >= offset)
+						.map(String::valueOf)
+						.collect(Collectors.joining("\n")),
+				primes.stream().map(String::valueOf).collect(Collectors.joining("\n")));
 	}
 }
