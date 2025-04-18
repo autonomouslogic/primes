a4ee9ee6795563a51fd74c23d59dde7cb4b36554
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-04-18 13:38:33 +0900

    Generate index HTML page (#9)

---
+++ src/main/java/com/autonomouslogic/primes/IndexHtml.java
@@ -0,0 +1,155 @@
+package com.autonomouslogic.primes;
+
+import com.autonomouslogic.primes.meta.IndexMeta;
+import com.autonomouslogic.primes.meta.PrimeFileMeta;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
+
+@RequiredArgsConstructor
+public class IndexHtml {
+	private static final DecimalFormat numberFormat = new DecimalFormat("#,###.#");
+	private static final DecimalFormat sizeFormat = new DecimalFormat("#,###.00");
+	private static final double kilo = 1 << 10;
+	private static final double mega = 1 << 20;
+	private static final double giga = 1 << 30;
+
+	private final IndexMeta indexMeta;
+
+	public String generate() {
+		return header() + table() + footer();
+	}
+
+	@SneakyThrows
+	public void generate(File file) {
+		try (var out = new FileOutputStream(file)) {
+			IOUtils.copy(new StringReader(generate()), out, StandardCharsets.UTF_8);
+		}
+	}
+
+	private String header() {
+		return """
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<script data-goatcounter="https://everef-data.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+				<title>Prime Numbers - data.kennethjorgensen.com</title>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+				<meta property="og:title" content="Prime Numbers - data.kennethjorgensen.com">
+				<meta property="twitter:title" content="Prime Numbers - data.kennethjorgensen.com">
+				<style>
+					.text-right { text-align: right; }
+					.text-left { text-align: left; }
+					.text-italic { font-style: italic; }
+					th, td { padding: 5px; }
+				</style>
+			</head>
+			<body>
+			<h1>Prime Numbers</h1>
+			""";
+	}
+
+	private String table() {
+		return String.format(
+				"""
+			<table>
+				<thead>
+					<tr>
+						<th class="text-left">File</th>
+						<th class="text-right">First prime</th>
+						<th class="text-right">Last prime</th>
+						<th class="text-right">Count</th>
+						<th class="text-right">Compressed size</th>
+						<th class="text-right">Uncompressed size</th>
+					</tr>
+				</thead>
+				<tbody>
+				%s
+				%s
+				</tbody>
+			</table>
+			""",
+				tableRows(), totalRow());
+	}
+
+	private String tableRows() {
+		return indexMeta.getPrimeFiles().stream()
+				.map(file -> String.format(
+						"""
+			<tr>
+				<td class="text-left"><a href="%s">%s</a></td>
+				<td class="text-right">%s</td>
+				<td class="text-right">%s</td>
+				<td class="text-right">%s</td>
+				<td class="text-right">%s</td>
+				<td class="text-right">%s</td>
+			</tr>
+			""",
+						file.getUrl(),
+						new File(URI.create(file.getUrl()).getPath()).getName(),
+						numberFormat.format(file.getFirstPrime()),
+						numberFormat.format(file.getLastPrime()),
+						numberFormat.format(file.getCount()),
+						formatSize(file.getCompressedSize()),
+						formatSize(file.getUncompressedSize())))
+				.collect(Collectors.joining("\n"));
+	}
+
+	private String totalRow() {
+		var totalPrimes = indexMeta.getPrimeFiles().stream()
+				.mapToLong(PrimeFileMeta::getCount)
+				.sum();
+		var totalCompressedSize = indexMeta.getPrimeFiles().stream()
+				.filter(f -> f.getCompressedSize() != null)
+				.mapToLong(PrimeFileMeta::getCompressedSize)
+				.sum();
+		var totalUncompressedSize = indexMeta.getPrimeFiles().stream()
+				.mapToLong(PrimeFileMeta::getUncompressedSize)
+				.sum();
+		return String.format(
+				"""
+			<tr>
+				<td class="text-left text-italic">Totals</td>
+				<td></td>
+				<td></td>
+				<td class="text-right text-italic">%s</td>
+				<td class="text-right text-italic">%s</td>
+				<td class="text-right text-italic">%s</td>
+			</tr>
+			""",
+				numberFormat.format(totalPrimes), formatSize(totalCompressedSize), formatSize(totalUncompressedSize));
+	}
+
+	private String footer() {
+		return """
+			<hr/>
+			<p>
+				<a href="primes.json">View JSON</a>
+				<a href="https://github.com/autonomouslogic/primes">Source Code</a>
+			</p>
+			</body>
+			</html>
+			""";
+	}
+
+	private String formatSize(Long size) {
+		if (size == null) {
+			return "";
+		}
+		if (size >= giga * 0.9) {
+			return sizeFormat.format(size / giga) + " GiB";
+		}
+		if (size >= mega * 0.9) {
+			return sizeFormat.format(size / mega) + " MiB";
+		}
+		return sizeFormat.format(size / kilo) + " KiB";
+	}
+}
