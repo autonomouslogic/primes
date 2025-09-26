f4e17e7b939ed9fc8d397031c56776d3cffcfb1b
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-09-26 13:48:01 +0900

    Working on automatic sieves

--- src/main/java/com/autonomouslogic/primes/PrimeSources.java
+++ src/main/java/com/autonomouslogic/primes/PrimeSources.java
@@ -1,5 +1,7 @@
 package com.autonomouslogic.primes;
 
+import java.util.function.Supplier;
+
 /**
  * Constructs prime sources.
  */
@@ -8,7 +10,26 @@ public class PrimeSources {
 	 * Returns unbounded prime source of all primes.
 	 * @return
 	 */
+	public static PrimeSource all(int memoryIncrements) {
+		var init = new PrimeList();
+		var next = nextSieve(init, memoryIncrements);
+		return new ConcatPrimeSource(init, next);
+	}
+
+	private static Supplier<PrimeSource> nextSieve(PrimeSource previous, int memoryIncrements) {
+		return () -> {
+			var start = previous.lastNumber() - previous.lastNumber() % 30;
+			var sieve = new SieveOfEratosthenes(start, memoryIncrements);
+			var initMemoryIncrements = (int) Math.sqrt(memoryIncrements);
+			var init = all(initMemoryIncrements);
+			sieve.init(init.primeStream());
+			sieve.run();
+			var current = new BoundedPrimeSource(sieve, previous.lastNumber() + 2, sieve.lastNumber());
+			return new ConcatPrimeSource(current, nextSieve(current, memoryIncrements));
+		};
+	}
+
 	public static PrimeSource all() {
-		return null;
+		return all(16 << 20);
 	}
 }
---
+++ src/test/java/com/autonomouslogic/primes/PrimeSourcesTest.java
@@ -0,0 +1,17 @@
+package com.autonomouslogic.primes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimeSourcesTest {
+	@Test
+	void shouldReturnAllPrimeNumbers() {
+		var expected =
+				new PrimalityTestPrimeSource(new TrialDivision()).primeStream().iterator();
+		var primes = PrimeSources.all().primeStream().filter(n -> n > 2).iterator();
+		for (int i = 0; i < 20_000; i++) {
+			assertEquals(expected.nextLong(), primes.nextLong());
+		}
+	}
+}
